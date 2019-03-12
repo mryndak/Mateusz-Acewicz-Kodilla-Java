5363281939aebf89e7b7b0ce8dfde446f0c24b68
MAcewicz <acewicz.m@gmail.com>
2019-03-12 19:57:17 +0000

    update do zadania 6.2

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,37 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
-    public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+    public static void main(String[] args) {
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        System.out.println("Test - pierwszy test jednostkowy:");
+
+        Calculator calc = new Calculator();
+
+        int add = calc.add(2, 4);
+        int subtract = calc.subtract(10, 7);
+
+        if (add == 6) {
+            System.out.println("Funkcja dodawania działa poprawnie");
+        } else {
+            System.out.println("Błąd w funkcji dodawania");
+        }
+
+        if (subtract == 3) {
+            System.out.println("Funkcja odejmowania działa poprawnie");
+        } else {
+            System.out.println("Błąd w funkcji odejmowania");
+        }
     }
 }
